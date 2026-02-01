e37fd8b3744a74873c1e197ccbbfcd3f526cbf56
bsc23me <bsc23me@yahoo.com>
2026-02-01 11:18:28 -0700

    implemented statements

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,7 +11,7 @@ public class GameManager : MonoBehaviour
     private PopulateGuestList populateGuestList;
 
     private int currentGuest;
-    private int totalGuestsAndIntruders;
+    [SerializeField] private int totalGuestsAndIntruders;
     private List<GuestProfile> roundProfiles;
 
     [SerializeField] private SpriteRenderer guestRenderer;
@@ -24,19 +25,25 @@ public class GameManager : MonoBehaviour
 
     private int guestsEntered;
 
+    [SerializeField] private TextMeshProUGUI guestStatements;
+
+    private List<string> statements;
+
     public void Awake()
     {
         if(instance == null)
             instance = this;
     }
-
+    // t
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         populateGuestList = GetComponent<PopulateGuestList>();
         roundProfiles = new List<GuestProfile>();
-        roundProfiles.AddRange(populateGuestList.profiles);
+        statements = new List<string>();
         GenerateRoundProfiles(totalGuestsAndIntruders);
+
+        SendNextGuest();
     }
 
     public void SelectGuest(int id)
@@ -44,6 +51,7 @@ public class GameManager : MonoBehaviour
         Debug.Log("Selected " + populateGuestList.CompleteProfileList[id].guestName);
         if (currentGuest == id)
         {
+            // TODO: punish for intruder
             guestsEntered++;
             if (guestsEntered < populateGuestList.numberOfGuests)
                 SendNextGuest();
@@ -63,27 +71,50 @@ public class GameManager : MonoBehaviour
             }
             else
             {
-                //GetNextStatement();
+                GetNextStatement();
             }
         }
     }
 
     void GenerateRoundProfiles(int numberOfGuests)
     {
+        roundProfiles.AddRange(populateGuestList.profiles);
+        //Debug.Log("RoundProfiles: "+roundProfiles.Count);
         int toRemove = roundProfiles.Count - numberOfGuests;
         for (int i = 0; i < toRemove; i++)
         {
             int j = Random.Range(0, roundProfiles.Count);
             roundProfiles.RemoveAt(j);
         }
+        //Debug.Log("RoundProfiles: "+roundProfiles.Count);
     }
 
     void SendNextGuest()
     {
         guestAnnoyances = 0;
         int j = Random.Range(0, roundProfiles.Count);
-
+        currentGuest = roundProfiles[j].id;
+        AddGuestStatements(roundProfiles[j]);
+        GetNextStatement();
     }
 
+    void AddGuestStatements(GuestProfile profile)
+    {
+        statements.Clear();
+        statements.AddRange(profile.statements);
+    }
 
+    void GetNextStatement()
+    {
+        if (statements.Count > 0)
+        {
+            int j = Random.Range(0, statements.Count);
+            guestStatements.text = statements[j];
+            statements.RemoveAt(j);
+        }
+        else
+        {
+            guestStatements.text = "...";
+        }
+    }
 }
